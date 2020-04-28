031f8d69832d78c2269a570b78073b2b96c202dc
Brandon <brandon@reddirtwebdesign.com>
2020-04-27 23:56:12 -0700

    Still trying to fix proxy issue

---
+++ src/lambda/Orders-Model.js
@@ -0,0 +1,25 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const schema = new mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	payPalOrderID: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	song: {
+		type: String,
+		required: true,
+	},
+});
+
+const Order = mongoose.model('orders', schema);
+
+export default Order;
